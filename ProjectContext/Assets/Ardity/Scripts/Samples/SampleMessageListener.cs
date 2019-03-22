09d1ba52065e08aadd71cd1ff10425beb024554d
Luuk Siewers <luuk@siewers.org>
2019-03-22 16:47:26 +0100

    Kmoest commit terughalen voor playtest. Bij deze reset die ik moet pushen om nieuwe updates te kunnen pullen

---
+++ ProjectContext/Assets/Ardity/Scripts/Samples/SampleMessageListener.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SampleMessageListener : MonoBehaviour
+{
+	private string readIncomingMessage;
+
+    void OnMessageArrived(string msg)
+    {
+        Debug.Log("Message arrived: " + msg);
+		readIncomingMessage = msg;
+    }
+
+	private void Update()
+	{
+		if(readIncomingMessage == "1")
+		{
+			SceneManager.LoadScene("Main");
+		}
+	}
+
+	void OnConnectionEvent(bool success)
+    {
+        if (success)
+            Debug.Log("Connection established");
+        else
+            Debug.Log("Connection attempt failed or disconnection detected");
+    }
+}
