b2f9c9924f4cdf2e64456e16518a79d7a608717f
DESKTOP-F6IC33L\Pimmez <pimhoreman@gmail.com>
2019-03-25 20:01:04 +0100

    -DELETED Old Script -> CharacterMovement.cs -Added PlayerController.cs -Added PlayerMotor.cs -Changed Main.unity -Added new refined cursor screen lock -Added vertical aim

---
+++ ProjectContext/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float lookSensitivityX = 5f;
+	[SerializeField] private float lookSensitivityY = 5f;
+
+	private PlayerMotor playerMotor;
+
+	private void Start()
+    {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		playerMotor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+		//Player Horizontal Rotation
+		float _yRot = Input.GetAxisRaw("Mouse X");
+
+		Vector3 _rotation = new Vector3(0, _yRot, 0) * lookSensitivityX;
+
+		playerMotor.Rotate(_rotation);
+
+		//Cam Vertical Rotation
+		float _xRot = Input.GetAxisRaw("Mouse Y");
+
+		Vector3 _camRotation = new Vector3(_xRot, 0, 0) * lookSensitivityY;
+
+		playerMotor.RotateCamera(_camRotation);
+	}
+}
---
+++ ProjectContext/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+	[SerializeField] private Camera cam;
+
+	private Vector3 velocity = Vector3.zero;
+	private Vector3 rotation = Vector3.zero;
+	private Vector3 camRotation = Vector3.zero;
+
+	private Rigidbody rb;
+
+	private void Start()
+    {
+		rb = GetComponent<Rigidbody>();
+    }
+
+	public void Rotate(Vector3 _rotation)
+	{
+		rotation = _rotation;
+	}
+
+	public void RotateCamera(Vector3 _camRotation)
+	{
+		camRotation = _camRotation;
+	}
+
+	private void FixedUpdate()
+	{
+		PerformRotation();
+	}
+
+	private void PerformRotation()
+	{
+		rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+		if(cam != null)
+		{
+			cam.transform.Rotate(-camRotation);
+		}
+	}
+}
