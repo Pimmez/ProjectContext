370ac7caea535fcaeeb88d0337481eafb0e48be7
DESKTOP-F6IC33L\Pimmez <pimhoreman@gmail.com>
2019-03-26 13:51:50 +0100

    -Added Soundmanager -Updated Main and other scripts

--- ProjectContext/Assets/Scripts/NPCBehaviour.cs
+++ ProjectContext/Assets/Scripts/NPCBehaviour.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class NPCBehaviour : MonoBehaviour
 {
-	public static Action NPCHitEvent;
+	public static Action NPCHitEvent; 
 
 	private void OnTriggerEnter(Collider col)
 	{
