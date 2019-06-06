6a750c1acc74127c7366881167da72c6041cd792
DESKTOP-F6IC33L\Pimmez <pimhoreman@gmail.com>
2019-06-06 12:29:34 +0200

    FIXED ANIMATION

--- ProjectContext/Assets/Scripts/EnemyBehaviour.cs
+++ ProjectContext/Assets/Scripts/EnemyBehaviour.cs
@@ -7,59 +7,76 @@ public class EnemyBehaviour : MonoBehaviour
 	public static Action EnemyDeadEvent;
 
 	[SerializeField] private Avatar grabAvatar;
+	[SerializeField] private Avatar danceAvatar;
+	[SerializeField] private Avatar loveAvatar;
+
 	private SoundManager sound;
 	private ParticleInstantiater particles;
 
-	[SerializeField] private Animator anim;
+	private Animator anim;
+
+	private float timer;
+	private float maxTimer = 8f;
+	private bool isHit;
 
 	private void Start()
 	{
 		sound = FindObjectOfType<SoundManager>();
 		particles = FindObjectOfType<ParticleInstantiater>();
+		anim = GetComponent<Animator>();
 	}
 
 	private void Update()
 	{
-		if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !anim.IsInTransition(0))
+		if(!isHit)
 		{
+			timer += Time.deltaTime;
 
-			StartCoroutine(WaitFewSeconds());
+			if (timer < maxTimer)
+			{
+				anim.SetTrigger("Dance");
+				anim.avatar = danceAvatar;
+			}
+
+			if (timer >= maxTimer)
+			{
+				anim.SetTrigger("Grab");
+				anim.avatar = grabAvatar;
+			}
 		}
 
-		//InvokeRepeating("GrabNPC", 2.0f, UnityEngine.Random.Range(3f, 10f));
+		if(isHit)
+		{
+			anim.SetTrigger("Bear");
+			anim.avatar = loveAvatar;
+		}
 	}
 
-	IEnumerator WaitFewSeconds()
+	public void EndOfGrab()
 	{
-		yield return new WaitForSeconds(UnityEngine.Random.Range(2f, 10f));
-		anim.SetTrigger("Grab");
-		anim.avatar = grabAvatar;
+		timer = 0;
+	}
 
+	private void EnemyAnimationIndex(int i, Avatar avatar)
+	{
+		anim.SetInteger("EnemySequence", i);
+		anim.avatar = avatar;
 	}
 
 	private void OnTriggerEnter(Collider col)
 	{
-		if (col.gameObject.tag == Tags.Bear)
+		if (col.gameObject.tag == Tags.Bear && anim.GetCurrentAnimatorStateInfo(0).IsName("Grab"))
 		{
+			isHit = true;
 			Destroy(col.gameObject);
 			sound.PlayAudio(2);
 			particles.InstanciateParticle(1, this.transform);
 
-			anim.gameObject.GetComponent<Animator>().enabled = false;
-
+			//Adds Points to UI
 			if (EnemyDeadEvent != null)
 			{
 				EnemyDeadEvent();
 			}
-
-			StartCoroutine(TimeTillDeath());
 		}
-	}
-
-	IEnumerator TimeTillDeath()
-	{
-		yield return new WaitForSeconds(1f);
-
-		Destroy(this.gameObject);
 	}
 }
